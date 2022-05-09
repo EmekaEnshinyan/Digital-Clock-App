98947ab8b7d686a7c30437d7404f271a9d6c4595
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-09 00:38:41 -0700

    edit: tick function

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-//Examine doc object 
-console.dir(document);
-console.log(document.domain);
-console.log(document.doctype);
+var HH = 00;
+var MM = 00;
+var SS = 00;
+var nom = document.getElementById("nominal");
 
-//get element by id
-var headerTitle = document.getElementById("header-title");
-//changes text. overrides any styling
-headerTitle.textContent = 'Emeka is Desert Power';
-//this is a better use since it outputs the stylizing from html file
-headerTitle.innerText = "Emeka: \"I am Desert Power\" ";
-//can use inner html so replacement will be placed inside original DOM el
-headerTitle.innerHTML = "We are Desert Power";
-console.log(headerTitle);
+
+function clockTick() {
+    
+    nom.innerText = HH + ":" + MM + ":" + SS + 1;
+}
+setInterval(clockTick, 1000);
+
+
+
+
+
+
+
+console.log(nom);
