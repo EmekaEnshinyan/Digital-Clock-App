a75a981b16003c1e998e7c48bad2b340c06af389
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-11 18:14:21 -0700

    edit: clock fix

--- App.js
+++ App.js
@@ -8,8 +8,21 @@ const hh = date.getHours();
 const mm = date.getMinutes();
 const ss = date.getSeconds();
 
-const time = `${hh}:${mm}:${ss}`;
-nominal.innerText = time;
+if (hh < 10){
+    const time = `0${hh}:${mm}:${ss}`;
+    nominal.innerText = time;
+}else if (mm < 10){
+    const time = `${hh}:0${mm}:${ss}`;
+    nominal.innerText = time;
+}else if (ss < 10){
+    const time = `${hh}:${mm}:0${ss}`;
+    nominal.innerText = time;
+}else{
+    const time = `${hh}:${mm}:${ss}`;
+    nominal.innerText = time;
+}
+
+
 
 today.innerText = now;
 
