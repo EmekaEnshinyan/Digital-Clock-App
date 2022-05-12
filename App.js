cbacf638e28ec9e06516bebdcfb6a78a130969f0
Enshinyan <emeka.enshinyan@gmail.com>
2022-05-11 17:24:00 -0700

    edit: time and date deliminated

---
+++ App.js
@@ -0,0 +1,21 @@
+const nominal = document.getElementById("clock");
+const today = document.getElementById("date");
+//cannot use date.toLocaleTimeString()
+function clockTick(){
+const date = new Date();
+const now = new Date().toDateString();
+const hh = date.getHours();
+const mm = date.getMinutes();
+const ss = date.getSeconds();
+
+const time = `${hh}:${mm}:${ss}`;
+nominal.innerText = time;
+
+
+today.innerText = now;
+
+setInterval(clockTick, 1000);
+}
+clockTick();
+
+    
